bc1d8f59d0e7df1b9630a102002745abe04bf840
agata-sm <smialowskaa@gmail.com>
2024-05-13 11:34:16 +0200

    update install R libraries

--- misc/docker/xenofilter/install_BioconductorLibs.R
+++ misc/docker/xenofilter/install_BioconductorLibs.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-# script to install Bioconductor R packages required for generation of reports by CRISPR-pooled-RSL pipeline
+# script to install Bioconductor R packages required for xenofilter
 # based on https://github.com/casbap/ncRNA/blob/main/docker/rpkgs.R
 # used in images based on rocker/tidyverse:4.2.3
 
@@ -8,8 +8,6 @@
 #     install.packages("BiocManager")
 # BiocManager::install(version = "3.16")
 
-# BiocManager::install(c('MAGeCKFlute','edgeR','fgsea','clusterProfiler','enrichplot','DOSE','biomaRt','ReactomePA','reactome.db','org.Hs.eg.db','org.Mm.eg.db'),
-# 	version = "3.16", ask=FALSE, update=FALSE)
 
 BiocManager::install(c('Rsamtools','GenomicAlignments','BiocParallel'),
 	ask=FALSE, update=FALSE)
